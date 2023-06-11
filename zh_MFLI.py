5a9929521d56f87fea13b4941b46e0c7c20d817a
FlyingCurryMonster <rakinb1998@gmail.com>
2023-06-11 13:51:39 -0400

    zi class

--- zh_MFLI.py
+++ zh_MFLI.py
@@ -13,30 +13,45 @@ class zi_MFLI:
                 server_port = 8004,
                 api_level = 6,):
 
+        self.device_id = device_id
 
         self.daq = zi.ziDAQServer(server_host, server_port, api_level)
-        self.instr.connectDevice(device_id, interface)
-        
-        self.demod0 =f'/{device_id}/demods/0/' 
-        self.sample0 = self.demod0+'sample'
-
-    def get_XY(self):
-        resp = self.daq.getSample(self.demod0+'sample')
+        self.daq.connectDevice(device_id, interface)
         
 
-# def get_X():
-#     return
-# def get_Y():
-#     return
-# def get_R():
-#     return
-# def get_theta():
-#     return
-# def get_TC():
-#     return
-# def get_filter():
-#     return
-# def set_TC():
-#     return
-# def set_filter():
-#     return 
+    def get_XY(self, demod:int = 0):
+        demod_path = f'/{self.device_id}/demods/{demod}/'
+        resp = self.daq.getSample(demod_path+'sample')
+        return [resp['x'], resp['y']]
+    
+    def get_freq(self, demod:int = 0):
+        demod_path = f'/{self.device_id}/demods/{demod}/'
+        resp = self.daq.getSample(demod_path+'sample')
+        return resp['frequency']
+
+    def set_freq(self, freq, demod:int = 0):
+        demod_path = f'/{self.device_id}/demods/{demod}/'
+        self.daq.setDouble('/dev4934/oscs/0/freq', freq)
+
+    def set_harmonic(self, harm:int, demod:int = 0):
+        demod_path = f'/{self.device_id}/demods/{demod}/'
+        self.daq.setInt(demod_path+'harmonic', harm)
+    
+    def set_phase(self, theta:float, demod:int=0):  #Sets phase in degrees
+        demod_path = f'/{self.device_id}/demods/{demod}/'
+        self.daq.setDouble(demod_path+'phaseshift', theta)
+    
+    def out_on(self, demod:int=0):
+        sigout_path = f'/{self.device_id}/sigouts/0/enables/{demod}'
+        self.daq.setInt(sigout_path, 1)
+    def out_off(self, demod:int=0):
+        sigout_path = f'/{self.device_id}/sigouts/0/enables/{demod}'
+        self.daq.setInt(sigout_path, 0)
+    
+    def set_amp(self, voltage:float, demod:int =0): #peak to peak
+        sigout_path = f'/{self.device_id}/sigouts/0/amplitudes/{demod}'
+        self.daq.setDouble(sigout_path, voltage)
+    
+    
+    
+        
