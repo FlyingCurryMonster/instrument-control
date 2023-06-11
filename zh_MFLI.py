b407cb743613ba335ca84ea7dbda9f6757a61612
FlyingCurryMonster <rakinb1998@gmail.com>
2023-06-11 10:14:05 -0400

    c

--- zh_MFLI.py
+++ zh_MFLI.py
@@ -10,28 +10,33 @@ class zi_MFLI:
                 device_id,
                 interface = '1GbE',
                 server_host = 'localhost',
-                # sever_host = '192.168.77.2',
                 server_port = 8004,
                 api_level = 6,):
 
 
         self.daq = zi.ziDAQServer(server_host, server_port, api_level)
         self.instr.connectDevice(device_id, interface)
-        self.spath = f'/{device_id}/demods/0/sample'
+        
+        self.demod0 =f'/{device_id}/demods/0/' 
+        self.sample0 = self.demod0+'sample'
 
-def get_X():
-    return
-def get_Y():
-    return
-def get_R():
-    return
-def get_theta():
-    return
-def get_TC():
-    return
-def get_filter():
-    return
-def set_TC():
-    return
-def set_filter():
-    return 
+    def get_XY(self):
+        resp = self.daq.getSample(self.demod0+'sample')
+        
+
+# def get_X():
+#     return
+# def get_Y():
+#     return
+# def get_R():
+#     return
+# def get_theta():
+#     return
+# def get_TC():
+#     return
+# def get_filter():
+#     return
+# def set_TC():
+#     return
+# def set_filter():
+#     return 
