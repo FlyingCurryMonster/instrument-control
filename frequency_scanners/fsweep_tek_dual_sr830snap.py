625fee276ec85bf5504fd7232cd5a47aeae71116
FlyingCurryMonster <64708836+FlyingCurryMonster@users.noreply.github.com>
2025-02-17 11:39:51 -0500

    dual sr830 measurements

--- frequency_scanners/fsweep_tek_dual_sr830snap.py
+++ frequency_scanners/fsweep_tek_dual_sr830snap.py
@@ -1,10 +1,8 @@
 import logging
-from matplotlib import units
-log = logging.getLogger(__name__)
-log.addHandler(logging.NullHandler())
 import sys
 import time
 import numpy as np
+import concurrent.futures
 from pymeasure.instruments.srs import SR830
 from pymeasure.display.Qt import QtWidgets
 from pymeasure.display.windows import ManagedWindow
@@ -12,48 +10,54 @@ from pymeasure.experiment import Procedure, Results, unique_filename
 from pymeasure.experiment import IntegerParameter, FloatParameter, Parameter
 from pymeasure.instruments.tektronix import AFG3152C
 from SR830_FAST2_PR.sr830_buffer import SR830Interface
-import concurrent.futures
 from pyvisa import VisaIOError
-class tek_dual_sr830_fsweep(Procedure):
 
-    drive_amp = FloatParameter('Drive Amplitude (Vpp)', default = 1)
-    f_start = FloatParameter('start frequency (Hz)', default = 21.5E3)
-    f_final = FloatParameter('stop frequency (Hz)', default = 22E3)
-    f_step = FloatParameter('Frequency Step (Hz)', default = 1)
-    delay = FloatParameter('Delay (s)', default = 3)
-    # buffer_time = FloatParameter('Buffer measure time (s)', default = 10 
-    tek_id = Parameter('Tek AFG addr', default = '0::1')
-    sr830_id_1 = Parameter('SR830_1 addr.', default = '0::12')
-    sr830_id_2 = Parameter('SR830_2 addr.', default = '2::9')
-    
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
+
+class TekDualSR830FSweep(Procedure):
+
+    drive_amp = FloatParameter('Drive Amplitude (Vpp)', default=1)
+    f_start = FloatParameter('Start Frequency (Hz)', default=21.5E3)
+    f_final = FloatParameter('Stop Frequency (Hz)', default=22E3)
+    f_step = FloatParameter('Frequency Step (Hz)', default=1)
+    delay = FloatParameter('Delay (s)', default=3)
+    # buffer_time = FloatParameter('Buffer measure time (s)', default=10)
+    tek_id = Parameter('Tek AFG addr', default='0::1')
+    sr830_id_1 = Parameter('SR830_1 addr.', default='0::12')
+    sr830_id_2 = Parameter('SR830_2 addr.', default='2::9')
+
     params = [
-        'drive_amp', 'f_start', 'f_final', 'f_step', 'delay', 
+        'drive_amp', 'f_start', 'f_final', 'f_step', 'delay',
         # 'buffer_time',
         'tek_id', 'sr830_id_1', 'sr830_id_2'
     ]
 
     DATA_COLUMNS = [
-        'UTC', 'timestamp', 'f', 
-        'X_1', #'sigma_x1', 
-        'Y_1', #'sigma_y2', 
-        'X_2', #'sigma_x2', 
-        'Y_2', #'sigma_y2'
-        ]
+        'UTC', 'timestamp', 'f',
+        'X_1',  # 'sigma_x1',
+        'Y_1',  # 'sigma_y2',
+        'X_2',  # 'sigma_x2',
+        'Y_2',  # 'sigma_y2'
+    ]
 
     def startup(self):
-        log.info('Starting frequency sweeper with DS345 drive, and measuring with two SR830s')
+        log.info('Starting frequency sweeper with DS345 drive, '
+                 'and measuring with two SR830s')
         tek_full_address = f'GPIB{self.tek_id}::INSTR'
         sr830_1_full_address = f'GPIB{self.sr830_id_1}::INSTR'
         sr830_2_full_address = f'GPIB{self.sr830_id_2}::INSTR'
 
+        log.info(f'Tek address: {tek_full_address}, '
+                 f'SR830 1 address: {sr830_1_full_address}, '
+                 f'SR830 2 address: {sr830_2_full_address}')
 
-        log.info(f'Tek address:{tek_full_address}, SR830 1 address: {sr830_1_full_address}, SR830 2 address: {sr830_2_full_address}')
-
-        self.afg =AFG3152C(tek_full_address)
+        self.afg = AFG3152C(tek_full_address)
         self.lockin_1 = SR830(sr830_1_full_address)
         self.lockin_2 = SR830(sr830_2_full_address)
 
-        self.freq = np.arange(self.f_start, self.f_final+self.f_step, self.f_step,)
+        self.freq = np.arange(self.f_start, self.f_final + self.f_step, self.f_step)
         self.t_start = time.time()
 
         self.afg.ch1.frequency = self.freq[0]
@@ -63,13 +67,12 @@ class tek_dual_sr830_fsweep(Procedure):
         time_constant_1 = self.lockin_1.time_constant
         time_constant_2 = self.lockin_2.time_constant
 
-        if self.delay < 10*time_constant_1 or self.delay<10*time_constant_2:
-            log.warn('Your delay should be at least 10 times the time constant')
+        if self.delay < 10 * time_constant_1 or self.delay < 10 * time_constant_2:
+            log.warning('Your delay should be at least 10 times the time constant')
         else:
-            log.info(f'Delay time is correctly >= 10 x the measurement time constant')
-        log.info(f'Lock-in 1 TC: {time_constant_1} and Lock-in 2 TC:{time_constant_2}')
+            log.info('Delay time is correctly >= 10 x the measurement time constant')
+        log.info(f'Lock-in 1 TC: {time_constant_1} and Lock-in 2 TC: {time_constant_2}')
 
-        
     def execute(self):
         for i, f in enumerate(self.freq):
             self.afg.ch1.frequency = f
@@ -77,8 +80,7 @@ class tek_dual_sr830_fsweep(Procedure):
             utc_time = time.time()
             ts = utc_time - self.t_start
             freq_meas = self.afg.ch1.frequency
-            log.info('Tek frequency set to ={} Hz'.format(freq_meas))
-
+            log.info(f'Tek frequency set to = {freq_meas} Hz')
 
             x1, y1 = self.lockin_1.xy
             x2, y2 = self.lockin_2.xy
@@ -92,33 +94,33 @@ class tek_dual_sr830_fsweep(Procedure):
                 'X_2': x2,
                 'Y_2': y2,
             }
-        
+
             self.emit('results', data)
-            self.emit('progress', 100*(i+1)/len(self.freq))
+            self.emit('progress', 100 * (i + 1) / len(self.freq))
             if self.should_stop():
                 log.warning('Caught the stop flag in the procedure')
-                break    
+                break
 
-class tek_dual_sr830_graph(ManagedWindow):
-    
+
+class TekDualSR830Graph(ManagedWindow):
+
     def __init__(self):
-
         super().__init__(
-            procedure_class=tek_dual_sr830_fsweep,
-            inputs =tek_dual_sr830_fsweep.params,
-            displays = tek_dual_sr830_fsweep.params,
-            x_axis= 'f',
-            y_axis = 'X_1',
-            directory_input = True
+            procedure_class=TekDualSR830FSweep,
+            inputs=TekDualSR830FSweep.params,
+            displays=TekDualSR830FSweep.params,
+            x_axis='f',
+            y_axis='X_1',
+            directory_input=True
         )
 
-        self.setWindowTitle('tek_dual_sr830_fsweep measurement window')
-        self.directory =  r'data-files/tek-dual-sr830'
+        self.setWindowTitle('TekDualSR830FSweep Measurement Window')
+        self.directory = r'data-files/tek-dual-sr830'
 
     def queue(self):
         directory = self.directory
-        filename = unique_filename(directory, prefix = 'tek_dual_sr830')
-        
+        filename = unique_filename(directory, prefix='tek_dual_sr830')
+
         procedure = self.make_procedure()
         results = Results(procedure, filename)
         experiment = self.new_experiment(results)
@@ -126,23 +128,17 @@ class tek_dual_sr830_graph(ManagedWindow):
         experiment.curve_list
         self.manager.queue(experiment=experiment)
 
+
 def main():
     # rm = pyvisa.ResourceManager()
     # gpib_list = rm.list_resources()
     # print(pymeasure.__version__)
-    
+
     app = QtWidgets.QApplication(sys.argv)
-    window = tek_dual_sr830_graph()
+    window = TekDualSR830Graph()
     window.show()
-    sys.exit(app.exec_()) 
+    sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
