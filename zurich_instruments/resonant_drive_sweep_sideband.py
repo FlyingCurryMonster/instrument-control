f5f3094750a7face8d8b523c97991ded3e3c5149
FlyingCurryMonster <rakinb1998@gmail.com>
2026-04-01 22:27:45 -0400

    changed default freq

--- zurich_instruments/resonant_drive_sweep_sideband.py
+++ zurich_instruments/resonant_drive_sweep_sideband.py
@@ -99,7 +99,7 @@ class ResonantDriveSweepSidebandProcedure(Procedure):
     fixed_sideband_demod_freq = FloatParameter(
         "Fixed sideband demod frequency",
         units="Hz",
-        default=1320.937981,
+        default=1.32093647e3,
         decimals=10,
         ui_class=HighPrecisionScientificInput,
     )
