52d59be98e06d51c6a32734062ba3c35c2c3bdba
FlyingCurryMonster <rakinb1998@gmail.com>
2026-04-25 22:53:51 -0400

    300mK background collection

--- zurich_instruments/off_resonance_background_console.py
+++ zurich_instruments/off_resonance_background_console.py
@@ -33,9 +33,9 @@ log.addHandler(logging.NullHandler())
 
 DEFAULT_RESULT_DIR = Path(r"D:/Data/Fall25-Summer26/TO off-resonance background")
 DEFAULT_TARGET_CSV = Path(__file__).with_name(
-    "off_resonance_targets_147mK_2026-04-24.csv"
+    "off_resonance_targets_320mK_2026-04-25.csv"
 )
-DEFAULT_RESONANCE_FREQUENCY = 1320.9285
+DEFAULT_RESONANCE_FREQUENCY = 1320.92035
 
 
 def parse_si_value(value) -> float:
